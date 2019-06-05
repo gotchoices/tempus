20997c9136666ef28e39591219ece93fbbbd93b3
JonahBateman <bateman.jonah@gmail.com>
2019-06-04 19:49:21 -0600

    added values.vue, updated buildings to be more scalable

---
+++ src/values.vue
@@ -0,0 +1,30 @@
+//Tempus Valorem - Values Bar
+//Copyright Kyle Bateman; all rights reserved
+// -----------------------------------------------------------------------------
+//TODO:
+//-
+
+<template>
+
+  <g class="building" v-show="showBuilding">
+
+  </g>
+
+</template>
+
+<script>
+
+export default {
+  name: 'tempus-values',
+  props: [],
+  data() { return {
+
+  }},
+  computed: {
+  },
+
+  methods: {
+  },
+}
+
+</script>
