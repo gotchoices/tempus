802522f69dc27c24d11f15c7df48a66501b5118b
JonahBateman <bateman.jonah@gmail.com>
2019-06-13 16:54:30 -0600

    Added timer.js and implemented a master timer

---
+++ src/timer.js
@@ -0,0 +1,28 @@
+//master Timer
+
+var timerID;
+var callbacks = {}
+var mSeconds = 10000
+
+module.exports={
+  start: function(mSeconds) {
+    timerId = setInterval(function() {
+      Object.keys(callbacks).forEach(id=>{
+        callbacks[id]()
+        //console.log("mSeconds", mSeconds)
+      })
+    }, mSeconds)
+  },
+
+  register: function(id, callback) {
+    if (callback)
+      callbacks[id] = callback
+    else if (id in callbacks)
+      delete callbacks[id]
+  },
+
+  stop: function() {
+    clearInterval(timerID)
+  },
+
+}
